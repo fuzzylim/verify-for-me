1d279fc8fb610584f34aa3e4706095b6d7fbd54a
Kelvin Lim <kelvin.lim@x15.com.au>
2023-12-01 07:53:07 +1100

    Handle abn and acn

--- lib/abr.ts
+++ lib/abr.ts
@@ -31,29 +31,41 @@ export interface AbnResponse {
 	Message: string;
 }
 
-export async function getBusinessDetails(abn: string): Promise<AbnResponse> {
+export async function getBusinessDetails(businessIdentifier: string): Promise<AbnResponse> {
 	const abnGuid = process.env.ABN_GUID;
 	if (!abnGuid) {
 		console.error('Error: ABN_GUID is not set in the environment.');
 		throw new Error('ABN_GUID is not set in the environment.');
 	}
 
-	const url = `https://abr.business.gov.au/json/AbnDetails.aspx?abn=${abn}&guid=${abnGuid}`;
+	// Detect and strip the prefix, and set the appropriate URL
+	let url;
+	if (businessIdentifier.startsWith("ABN")) {
+		const abn = businessIdentifier.substring(3); // Remove "ABN" prefix
+		url = `https://abr.business.gov.au/json/AbnDetails.aspx?abn=${abn}&guid=${abnGuid}`;
+	} else if (businessIdentifier.startsWith("ACN")) {
+		const acn = businessIdentifier.substring(3); // Remove "ACN" prefix
+		url = `https://abr.business.gov.au/json/AcnDetails.aspx?acn=${acn}&guid=${abnGuid}`; // Assuming a different endpoint for ACN
+	} else {
+		throw new Error('Invalid business identifier. Must start with ABN or ACN.');
+	}
 
-	console.debug('Fetching ABN details from:', url);
+	console.debug('Fetching business details from:', url);
 
 	try {
 		const response = await fetch(url);
 		if (!response.ok) {
-			throw new Error(`Error: ${response.status}`);
+			throw new Error(`Error fetching business details. Status: ${response.status}`);
 		}
-		const data = await response.text();
+		const data = await response.json();
 
-		// The response is wrapped in a callback function, so we need to extract the JSON part
-		const jsonStr = data.substring(data.indexOf('{'), data.lastIndexOf('}') + 1);
-		return JSON.parse(jsonStr) as AbnResponse;
+		if (!data || data.error) {
+			throw new Error(`Error in response data: ${data.error || 'Unknown error'}`);
+		}
+
+		return data as AbnResponse;
 	} catch (error) {
-		console.error('Error fetching ABN details:', error);
+		console.error('Error fetching business details:', error);
 		throw error;
 	}
 }
