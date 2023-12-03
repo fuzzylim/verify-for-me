0ebe074e22fbd0f150ad163ae36b367f62cc5291
Kelvin Lim <kelvin.lim@x15.com.au>
2023-12-04 09:31:58 +1100

    add test

--- lib/handleVerification.test.ts
+++ lib/handleVerification.test.ts
@@ -3,14 +3,22 @@ import { PrismaClient } from '@prisma/client';
 import { handleVerification, InvalidVerificationRequest } from './handleVerification';
 
 
+// Spy on prisma client
+// jest.spyOn(prisma.business, 'findFirst').mockResolvedValue(null);
+// jest.spyOn(prisma.trustedDomain, 'findFirst').mockResolvedValue({ id: '1', domain: '', abn: 'Test Business 1' } as any);
+// jest.spyOn(prisma.verificationCode, 'findFirst').mockResolvedValue({} as any);
+
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 // Mock dependencies
 jest.mock('@prisma/client', () => {
     return {
         PrismaClient: jest.fn().mockImplementation(() => {
             return {
                 business: {
-                    findFirst: jest.fn().mockResolvedValue(null),
                     create: jest.fn().mockResolvedValue({}),
+                    findFirst: jest.fn().mockResolvedValue(null)
                 },
                 verificationCode: {
                     findFirst: jest.fn().mockResolvedValue({}),
@@ -21,22 +29,17 @@ jest.mock('@prisma/client', () => {
                 },
                 trustedDomain: {
                     create: jest.fn().mockResolvedValue({}),
-                    findFirst: jest.fn().mockResolvedValue({ id: 1, domain: '', abn: 'Test Business 1' })
+                    findFirst: jest.fn().mockResolvedValue({})
                 },
             };
         }),
     };
 });
 
-
-
+const prisma = new PrismaClient(); // Reinitialize the prisma client for each test
 
 // Test suite
 describe('handleVerification', () => {
-    beforeEach(() => {
-        prisma = new PrismaClient(); // Reinitialize the prisma client for each test
-        jest.clearAllMocks();
-    });
 
     it('should throw InvalidVerificationRequest when emailAddress or subject is missing', async () => {
         await expect(handleVerification('', '')).rejects.toThrow(InvalidVerificationRequest);
